5332b91810e6e940447370f17fffd132d7f20a26
Saurabh Sharma <anshulsharma7665@gmail.com>
2025-09-19 00:05:28 +0530

    Implemented LangSmith integration into our project to trace and monitor our project. Refactor chatbot backend to use SQLite for persistence and enhance frontend with thread management features

--- langgraph_chatbot_backend.py
+++ langgraph_chatbot_backend.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, BaseMessage
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated
 from langgraph.graph.message import add_messages
+import sqlite3
 
 load_dotenv()
 
@@ -24,6 +25,22 @@ graph.add_node('chat_node', chat_node)
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
-checkpointer = InMemorySaver()
+connection = sqlite3.connect('chatbot.db', check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=connection, )
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        config = checkpoint.config
+        if 'thread_id' in config.get('configurable', {}):
+            all_threads.add(config['configurable']['thread_id'])
+    return list(all_threads)
+
+# config = {'configurable': {'thread_id': 'default_thread'}}
+
+# res = chatbot.invoke({'messages': [HumanMessage(content="what is capital of India and acknowledge my name also")]}, config=config)
+
+# print(res)
--- langgraph_chatbot_frontend.py
+++ langgraph_chatbot_frontend.py
@@ -1,11 +1,59 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage
-from langgraph_chatbot_backend import chatbot
+from langgraph_chatbot_backend import chatbot, retrieve_all_threads
+import uuid
 
-config = {'configurable': {'thread_id': 'thread_id_1'}}
+def generate_thread_id():
+    return str(uuid.uuid4())
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['message_history'] = []
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+    st.session_state['thread_id'] = thread_id
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def load_conversation(thread_id):
+    st.session_state['thread_id'] = thread_id
+    state = chatbot.get_state({'configurable': {'thread_id': thread_id}})
+    messages = state.values.get('messages', [])
+    st.session_state['message_history'] = [{'role': 'user' if isinstance(msg, HumanMessage) else 'assistant', 'content': msg.content} for msg in messages]
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = retrieve_all_threads()
+
+# config = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+config = {
+    'configurable': {'thread_id': st.session_state['thread_id']},
+    'metadata': {'thread_id': st.session_state['thread_id']},
+    'run_name': 'chat_turn'
+} # to create new thread for every new conversation in LangSmith
+
+add_thread(st.session_state['thread_id'])
+
+# *********************Sidebar section************************
+
+st.sidebar.title("LangGraph Chatbot")
+
+if st.sidebar.button("New Chat"):
+    reset_chat()
+
+st.sidebar.header("My Conversations")
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(thread_id):
+        load_conversation(thread_id)
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
